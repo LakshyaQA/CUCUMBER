f404e3244384a8293730037e4e463f6e5eff43c8
LakshyaSharmaQA <lakshyasharmaqa@gmaiol.com>
2023-03-31 22:21:50 +0530

    new

--- src/test/java/definingsteps/StepsLogin.java
+++ src/test/java/definingsteps/StepsLogin.java
@@ -7,46 +7,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
-
 public class StepsLogin {
-
     public static WebDriver driver;
 
     @Given("I navigate to login page")
     public void i_navigate_to_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "G:\\selinium\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver(options);
-
-
-        /*ChromeOptions ops;
-        ops = new ChromeOptions();
-        ops.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver", "G:/selinium/chromedriver_win32/chromedriver.exe/");
-        WebDriver driver = new ChromeDriver(ops) ;
-        ChromeOptions options;
-        ops = new ChromeOptions();
-        ops.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver", "G:\\selinium\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver(ops);*/
-
-
     }
-
     @When("i have valid id & password")
     public void i_have_id_and_password() {
-        // Write code here that turns the phrase above into concrete actions
-        driver.get("https://www.facebook.com/");
 
     }
-
     @Then("i can login should see dashboard")
     public void i_can_login_should_see_dashboard() {
-
+        driver.get("https://www.facebook.com/");
         driver.close();
 
     }
